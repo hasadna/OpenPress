27069f92803de79deba3a7239c9d2a5b3e737a36
Eshed Shaham <ashdnazg@gmail.com>
2014-01-08 20:15:45 +0200

    sorta working

--- sitemap_index.py
+++ sitemap_index.py
@@ -1,48 +1,53 @@
 import sys
 import zipfile
-import os
+import os.path
 #import xml.etree.ElementTree as ET
 
-path="????"
-date='datattttt'
+ROOT_PATH = "root"
 
-
-INDEX ='<?xml version="1.0" encoding="UTF-8"?>\n<sitemapindex xmlns="{PATH}/sitemap/0.9">'
-
-PREFIX="""
-   <sitemap>
-
-      <loc>"""
-     
-POSTFIX = """ </loc>
-     <lastmod>{DATE}</lastmod>
-
-   </sitemap>
+SITEMAP_INDEX_TEMPLATE="""<?xml version="1.0" encoding="UTF-8"?>
+<sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+{ENTRIES}</sitemapindex>
 """
 
-DEFAULT_DATE = "2013-01-01"
-DEFAULT_CHANGEFREQ = "monthly"
+ENTRY_TEMPLATE = """    <sitemap>
+        <loc>{LOC}</loc>
+        <lastmod>{LASTMOD}</lastmod>
+    </sitemap>
+"""
 
-def index_sitemaps(sitemaps):
+LOC_TEMPLATE = "/".join([ROOT_PATH, "{SITEMAP_PATH}"])
+
+DEFAULT_DATE = "2013-01-01"
+
+(SITEMAP_LOC,
+ SITEMAP_LASTMOD) = range(2)
+
+def get_loc(path):
+    return LOC_TEMPLATE.format(SITEMAP_PATH=path)
+
+def get_sitemap(path, date = DEFAULT_DATE):
+    return get_loc(path), date
+
+def create_entries(sitemaps):
     '''
     Index the sitemaps 
     '''
-    global INDEX
-  
-    for sitemap in sitemaps:
-        INDEX+= PREFIX+os.path.join('{PATH}',sitemap)+POSTFIX
+    for loc, lastmod in sitemaps:
+        yield ENTRY_TEMPLATE.format(LOC=loc, LASTMOD=lastmod)
         
-    INDEX.format(PATH=path , DATE = date)
-        
-    INDEX+="""</sitemapindex>"""    
-    print (INDEX)
-        
-        
+def get_sitemaps_from_folder(folder_path):
+    from os import walk
+    real_path = os.path.realpath(folder_path)
+    
+    for root, dirs, files in walk(real_path):
+        for file_name in files:
+            if file_name.lower().endswith(".xml"):
+                yield get_sitemap(os.path.relpath(os.path.join(root, file_name), real_path).replace("\\", "/"))
 
 def main(argv):
-    
-    INDEX = (index_sitemaps(argv[1:]))
-    print INDEX
+    sitemap_index = SITEMAP_INDEX_TEMPLATE.format(ENTRIES = "".join(create_entries(get_sitemaps_from_folder("."))))
+    print sitemap_index
 
 
 
