67926a0a0744b4dffe5fc0cb6666056d0086294e
Asaf Valadarsky <mestrebisli@gmail.com>
2014-01-08 19:47:57 +0200

    Initial Commit

---
+++ ocr/OCR Test/OCR Test/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  OCR Test
+//
+//  Created by Asaf Valadarsky on 1/8/14.
+//  Copyright (c) 2014 Asaf Valadarsky. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
