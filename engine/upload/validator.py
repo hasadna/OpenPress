88460146a6f9d2f6c9d713b70c48cdbf20b9e1a6
Yaniv Pascal <yanivpas@gmail.com>
2014-11-26 19:55:45 +0000

    Small changes to the validator

--- engine/upload/validator.py
+++ engine/upload/validator.py
@@ -9,13 +9,16 @@ import urllib2
 
 
 SERVER = "opa.org.il"
+g_logfile = None
 
 class ArticleError(Exception): pass
 
+def write_to_log(message):
+    g_logfile.write(message + '\n')
 
 def validate_article(article):
     
-
+    print article['id']
     # Set the request URL
     url = 'http://%s/api/v1/?articleId=%s' %(SERVER, article['id'])
 
@@ -30,7 +33,7 @@ def validate_article(article):
         articles = []
         for article in  data['results']:
             articles.append(article['id'])
-        raise ArticleError("The count for %s is different then one the list - %s" %(article['id'],str(articles)))
+        write_to_log("The count for %s is different then one the list - %s" %(article['id'],str(articles)))
 
 def validate_articles(articles):
     for article in articles:
@@ -44,10 +47,18 @@ class DemoSolr(object):
 
 
 
-def main(argv):
-    solr = DemoSolr()
-    upload_all(solr, argv[1])
+def main(path, log_filename):
+    global g_logfile
 
+    g_logfile = open(log_filename, 'a')
+    solr = DemoSolr()
+    upload_all(solr, path)
+
+def usage():
+    print "python %s <path_to_archive> <log_filename>" %(sys.argv[0],)
 
 if __name__ == "__main__":
-    main(sys.argv)
+    if len(sys.argv) != 3:
+        usage()
+    else:
+        main(*sys.argv[1:])
